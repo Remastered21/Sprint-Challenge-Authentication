d0b7f6516e0af138c869859bcfecd276d9275ea5
Henry K <mykim1891@hotmail.com>
2018-05-18 10:04:52 -0700

    Routes are set

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+import { Route, Redirect } from "react-router-dom";
+
+import Jokes from "./components/Jokes"
+import Login from "./components/Login"
+import Register from "./components/Register"
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1> Welcome </h1>
+        <Route
+          path="/"
+          render={() => {
+            <Redirect to="/api/login" />;
+          }}
+        />
+        <Route path="/api/" Component={} />
+        <Route path="/api/" Component={} />
+        <Route path="/api/" Component={} />
       </div>
     );
   }
